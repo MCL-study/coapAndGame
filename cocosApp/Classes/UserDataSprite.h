795b19b6c795e57a606519aac97473a7f3fc2c35
sylphe <myks790@gmail.com>
2016-05-09 23:49:44 +0900

    추적자나 도망자 sprite에 name label 추가

--- cocosApp/Classes/UserDataSprite.h
+++ cocosApp/Classes/UserDataSprite.h
@@ -1,4 +1,4 @@
-#ifndef __USER_DATA_SPRITE_H_
+﻿#ifndef __USER_DATA_SPRITE_H_
 #define __USER_DATA_SPRITE_H_
 #include "cocos2d.h"
 #include "UserData.h"
@@ -8,6 +8,7 @@ class UserDataSprite : public UserData{
 private:
 	cocos2d::Sprite* sprite = NULL;
 	bool aliveFlag = true;
+	cocos2d::Label* nameLabel = NULL;
 	cocos2d::Label* distanceLabel = NULL;
 
 public:
@@ -18,6 +19,15 @@ public:
 		return aliveFlag;
 	}
 	UserDataSprite(UserData user):UserData(user) {
+		int properties = user.getUserProperties();
+		std::string propertiesName;
+		if (properties == UserProperties::FUGITIVE)
+			propertiesName = "도망자";
+		else if (properties == UserProperties::CHASER)
+			propertiesName = "추적자";
+		nameLabel = cocos2d::Label::createWithTTF(propertiesName, "fonts/NanumPen.ttf", 30);
+		nameLabel->setAnchorPoint(cocos2d::Vec2(0.0, 0.5));
+		nameLabel->setColor(cocos2d::Color3B(0, 0, 0));
 		distanceLabel = cocos2d::Label::createWithTTF("", "fonts/NanumPen.ttf", 30);
 		distanceLabel->setAnchorPoint(cocos2d::Vec2(0.5, 0.5));
 		distanceLabel->setColor(cocos2d::Color3B(0, 0, 0));
@@ -32,7 +42,8 @@ public:
 	}
 	void addChild(cocos2d::Layer* context,int layer) {
 		context->addChild(sprite, layer);
-		context->addChild(distanceLabel,layer);
+		sprite->addChild(nameLabel, layer+2);
+		context->addChild(distanceLabel, layer+1);
 	}
 	void setSprite(cocos2d::Sprite* sprite) {
 		this->sprite = sprite;
