9879a14e25de6b0da818c28ab33d99dfe21adb81
sylphe <myks790@gmail.com>
2016-05-27 02:41:22 +0900

    메시지 타입 추가

--- DTO/src/main/java/com/sylphe/app/dto/MsgType.java
+++ DTO/src/main/java/com/sylphe/app/dto/MsgType.java
@@ -5,14 +5,15 @@ package com.sylphe.app.dto;
  */
 public class MsgType {
     public static final int MAKE_ROOM = 100;
-    public static final int REQUEST_ID = 103;
-    public static final int SEND_LOCATION = 104;
+ //   public static final int REQUEST_ID = 103;
+ //   public static final int SEND_LOCATION = 104;
     public static final int USER_DATA = 105;
 //    public static final int SEND_LOCATIONS = 105;
-    public static final int REQUEST_ROOM_LIST = 106;
+ //   public static final int REQUEST_ROOM_LIST = 106;
     public static final int ENTER_ROOM = 108;
     public static final int CATCH_FUGITIVE = 109;
     public static final int DIE_PLAYER = 110;
+    public static final int EXIT_USER =111;
     //--delete
    // public static final int RESPOND_ID = 102;
    // public static final int MAKE_ROOM_OK= 101;
