8cf182fe8eb07330e19cb96f3819698a6893548f
sylphe <myks790@gmail.com>
2016-05-31 12:36:12 +0900

    클라이언트에 timeout 구현

--- AndroidClientLibrary/src/main/java/com/shylphe/lib/android/client/ListenerResource.java
+++ AndroidClientLibrary/src/main/java/com/shylphe/lib/android/client/ListenerResource.java
@@ -1,12 +1,13 @@
 package com.shylphe.lib.android.client;
 
+import com.sylphe.app.dto.MsgType;
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 
 /**
  * Created by myks7 on 2016-05-27.
  */
-class ListenerResource extends CoapResource {
+abstract class ListenerResource extends CoapResource {
 
     ListenerResource() {
         // set resource identifier
@@ -16,9 +17,13 @@ class ListenerResource extends CoapResource {
     }
 
     @Override
-    public void handleGET(CoapExchange exchange) {
-
-        // respond to the request
-        exchange.respond("Hello Android!");
+    public void handlePUT(CoapExchange exchange) {
+        int contentFormat = exchange.getRequestOptions().getContentFormat();
+        if(contentFormat == MsgType.TIME_OUT){
+            System.out.println("time out");
+            onTimeout();
+        }
     }
+    abstract void onTimeout();
+
 }
